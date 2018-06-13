bfd941b4f47c4d390561110a10f873f4067d97b4
Youngsup Oh <ohing504@gmail.com>
2018-06-13 18:03:42 +0900

    Change pyenv installation to using git repo

--- roles/python/pyenv.zsh
+++ roles/python/pyenv.zsh
@@ -1,6 +1,8 @@
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 
+[ -s "$PYENV_ROOT/completions/pyenv.zsh" ] && \. "$PYENV_ROOT/completions/pyenv.zsh"
+
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
